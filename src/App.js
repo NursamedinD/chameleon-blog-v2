490595088cd3dcc339b1a4c378591e9e2f244c91
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-11-17 14:01:41 +0300

    changed some component names, added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import Header from
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import PostBlog from "./components/PostBlog";
+import NewPostForm from "./components/NewPostForm";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<PostBlog />} />
+          <Route path="/new" element={<NewPostForm />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
