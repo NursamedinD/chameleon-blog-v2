c429a00f6df000afb69a46def9019b8db2369fe2
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-11-17 16:11:21 +0300

    added more code to the PostBlog component

--- src/components/PostBlog.js
+++ src/components/PostBlog.js
@@ -2,8 +2,23 @@ import React, { useEffect, useState }from 'react';
 import { useParams } from 'react-router-dom';
 
 function PostBlog() {
+  const [posts, setPosts] = useState([])
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch("http://localhost:5000/posts")
+    .then((response) => response.json())
+    .then((data) => setPosts(data));
+  }, [id]);
+
+  if (!post) return <p>Loading...may take a bit</p>;
+
   return (
-    <div>PostBlog</div>
+    <div>
+      <h2>{post.title}</h2>
+      <img src={post.imageUrl} alt={post.title} style={{ width: "200px" }} />
+      <p>{post.content}</p>
+    </div>
   )
 }
 
