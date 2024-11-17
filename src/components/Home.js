816385adb9f924896e6db7a700f1c66440a5b0a4
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-11-17 15:42:46 +0300

    added Home component with needed info

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect} from 'react'
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/posts")
+    .then((response) => response.json())
+    .then((date) => setPosts(data));
+  }, [])
+
+
+  return (
+    <div>
+      <h1>Chameleon Blog</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+          <img src={post.imageUrl} alt={post.title} style={{ width: "200px" }} />
+          <Link to={`/post/${post.id}`}>Read More</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home
